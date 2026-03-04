48b2c355d24a9efbac56bc6c9e7a94971474f7af
Siddharth Mohanty <neosiddharth@gmail.com>
2026-03-04 14:44:24 -0800

    fix: theming

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Siddharth\'s C++ & Systems Blog';
+export const SITE_DESCRIPTION = 'WG21 insider perspectives, Catalyst ecosystem, and the reality of C++ build systems.';
